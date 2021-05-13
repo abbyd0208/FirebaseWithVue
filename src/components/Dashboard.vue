9e219e3059242e2af07506aaaceb111c1a95cce7
Abby <abby@circles.tw>
2021-05-13 17:28:46 +0800

    後台介面版型新增

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,289 @@
 <template>
-    <div class="container">
-        我是管理介面
+    <div class="admin">
+        <header>
+            <h4>任務旋轉門後台</h4>
+        </header>
+        <div class="container-fluid">
+             <div class="container">
+                 <h5>任務列表</h5>
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                        <th scope="col" width="10%">id</th>
+                        <th scope="col">標題</th>
+                        <th scope="col">公司名稱</th>
+                        <th scope="col">刊登開始</th>
+                        <th scope="col">刊登結束</th>
+                        <th scope="col">上/下架</th>
+                        <th scope="col" width="10%">操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                            <td>Linda</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>  <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>  <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>  <tr>
+                            <th scope="row">3</th>
+                            <td>Sandy</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td> <button type="button" class="btn btn-primary btn-sm">編輯</button>
+                                <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 
